53a4f354c9b8fe059133a8ffd827d7b14b08e00f
FelixRoeben <felix.roeben@haw-hamburg.de>
2020-10-27 11:22:21 +0100

    comments and changes on Auswertung.py

--- WC_data/Auswertung.py
+++ WC_data/Auswertung.py
@@ -6,8 +6,8 @@ import os
 # Identify location of files and set path
 # ===============================================================================
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path_1 = "results/WC_sim_output_period_reference.csv"
-rel_path_2 = "results/WC_sim_output_all_reference.csv"
+rel_path_1 = "results/1 no SB/WC_sim_output_period.csv"
+rel_path_2 = "results/1 no SB/WC_sim_output_all.csv"
 abs_file_path_period = os.path.join(script_dir, rel_path_1)
 abs_file_path_all = os.path.join(script_dir, rel_path_2)
 
@@ -27,13 +27,13 @@ reference_data_all.index = pd.date_range(start='00:00 01.01.2019', end = '23:59 
 # ===============================================================================
 # Load scenario 15 min (
 # ===============================================================================
-scenario_data = pd.read_csv("WC_sim_output_period.csv",sep=';',encoding='latin-1').round(1)
+scenario_data = pd.read_csv("results/2 All NL/WC_sim_output_period.csv",sep=';',encoding='latin-1').round(1)
 scenario_data.index = pd.date_range(start='00:00 01.01.2019', end = '23:30 12.31.2019',freq='15 min')
 
 # ===============================================================================
 # Load scenario 1 min
 # ===============================================================================
-scenario_data_all = pd.read_csv("WC_sim_output_all.csv",sep=';',encoding='latin-1').round(1)
+scenario_data_all = pd.read_csv("results/2 All NL/WC_sim_output_all.csv",sep=';',encoding='latin-1').round(1)
 scenario_data_all.index = pd.date_range(start='00:00 01.01.2019', end = '23:59 12.31.2019',freq='1 min')
 
 # ===============================================================================
@@ -43,6 +43,7 @@ scenario_data_all.index = pd.date_range(start='00:00 01.01.2019', end = '23:59 1
 start = "2019-01-01 0:00"
 end = "2019-01-01 1:30"
 
+reference_data_all[start:end].drop(['time [s]','f [Hz]','aFRR FRCE (open loop) [MW]','mFRR P [MW]','Unnamed: 11'],axis=1).plot(secondary_y='AEP [EUR/MWh]')
 scenario_data_all[start:end].drop(['time [s]','f [Hz]','aFRR FRCE (open loop) [MW]','mFRR P [MW]','Unnamed: 11'],axis=1).plot(secondary_y='AEP [EUR/MWh]')
 
 # ===============================================================================
@@ -51,9 +52,9 @@ scenario_data_all[start:end].drop(['time [s]','f [Hz]','aFRR FRCE (open loop) [M
 scenario_sum = scenario_data.sum()
 
 income_all = {}
-income_all['Solar'] = [scenario_sum['Solar AEP costs [EUR]'] - scenario_sum['Solar Marktprämie [EUR]']]
-income_all['Wind onshore'] = [scenario_sum['Wind onshore AEP costs [EUR]'] - scenario_sum['Wind onshore Marktprämie [EUR]']]
-income_all['Wind offshore'] = [scenario_sum['Wind offshore AEP costs [EUR]'] - scenario_sum['Wind offshore Marktprämie [EUR]']]
+income_all['Solar'] = [scenario_sum['Solar AEP costs [EUR]'] ]#- scenario_sum['Solar Marktprämie [EUR]']]
+income_all['Wind onshore'] = [scenario_sum['Wind onshore AEP costs [EUR]']]# - scenario_sum['Wind onshore Marktprämie [EUR]']]
+income_all['Wind offshore'] = [scenario_sum['Wind offshore AEP costs [EUR]']]# - scenario_sum['Wind offshore Marktprämie [EUR]']]
 income_all['Alu'] = [scenario_sum['Aluminium AEP costs [EUR]']]
 income_all['Alu'] = [scenario_sum['Aluminium AEP costs [EUR]']]
 income_all['Steel'] = [scenario_sum['Steel AEP costs [EUR]']]
@@ -68,7 +69,7 @@ print('------------------------------------------------------------------')
 print('PROFITS BY TECHNOLOGY')
 print('------------------------------------------------------------------')
 print('The balancers profit in kEUR')
-print(data/1000)
+print((data/1000).round(1))
 print('------------------------------------------------------------------')
 
 
@@ -78,10 +79,15 @@ print('------------------------------------------------------------------')
 # compare positive and negative balancing power of ref and sce
 aFRR_pos = scenario_sum['GER pos. energy aFRR [MWh]'] / reference_sum['GER pos. energy aFRR [MWh]']
 aFRR_neg = scenario_sum['GER neg. energy aFRR [MWh]'] / reference_sum['GER neg. energy aFRR [MWh]']
+mFRR_pos = scenario_sum['GER pos. energy mFRR [MWh]'] / reference_sum['GER pos. energy mFRR [MWh]']
+mFRR_neg = scenario_sum['GER neg. energy mFRR [MWh]'] / reference_sum['GER neg. energy mFRR [MWh]']
 print('POWER')
 print('------------------------------------------------------------------')
-print('Positive balancing power could be reduced by: ', aFRR_pos)
-print('Negative balancing power could be reduced by: ', aFRR_neg)
+print('Positive aFRR could be reduced by: ', aFRR_pos.round(2))
+print('Negative aFRR could be reduced by: ', aFRR_neg.round(2))
+print('------------------------------------------------------------------')
+print('Positive mFRR could be reduced by: ', mFRR_pos.round(2))
+print('Negative mFRR could be reduced by: ', mFRR_neg.round(2))
 print('------------------------------------------------------------------')
 # ===============================================================================
 # Calculate Overall System Costs
@@ -95,9 +101,9 @@ costs_neg = (scenario_sum['GER neg. aFRR costs [EUR]'] + scenario_sum['GER neg. 
 costs_tot = (scenario_sum['GER pos. aFRR costs [EUR]'] + scenario_sum['GER pos. mFRR costs [EUR]'] + scenario_sum['GER neg. aFRR costs [EUR]'] + scenario_sum['GER neg. mFRR costs [EUR]']) / (reference_sum['GER pos. aFRR costs [EUR]'] + reference_sum['GER pos. mFRR costs [EUR]'] + reference_sum['GER neg. aFRR costs [EUR]'] + reference_sum['GER neg. mFRR costs [EUR]'])
 print('COSTS')
 print('------------------------------------------------------------------')
-print('Pos balancing costs could be reduced by: ', costs_pos)
-print('Neg balancing costs could be reduced by: ', costs_neg)
-print('Total balancing costs could be reduced by: ', costs_tot)
+print('Pos balancing costs could be reduced by: ', costs_pos.round(2))
+print('Neg balancing costs could be reduced by: ', costs_neg.round(2))
+print('Total balancing costs could be reduced by: ', costs_tot.round(2))
 print('------------------------------------------------------------------')
 
 # ===============================================================================
