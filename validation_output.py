ce1106ce19eaf105b465c18a4356839ba8e67c79
FelixRoeben <felix.roeben@haw-hamburg.de>
2020-11-02 23:40:51 +0100

    validation: correlation between "no SB" scenario with historic time series (15min resol.) after change of AEP calc. corr of FRR energy and demand reamins, corr of AEP was improved

--- validation_output.py
+++ validation_output.py
@@ -26,9 +26,8 @@ no_SB_no_mFRR = pd.read_csv("validation_and_analysis/2019, 1min, no mFRR, no SB/
 # simulation data without Smart Balancing for validation - with mFRR
 no_SB_with_mFRR = pd.read_csv("validation_and_analysis/2019, 1min, with mFRR, no SB/WC_Sim_output_period.csv",sep=';')
 
-no_SB_FR = pd.read_csv("validation_and_analysis/results Felix/1 no SB/WC_Sim_output_period.csv",sep=';')
-fuzzySolar_SB_FR = pd.read_csv("validation_and_analysis/results Felix/2 Solar fuzzy SB/WC_Sim_output_period.csv",sep=';')
-Solar_SB_FR = pd.read_csv("validation_and_analysis/results Felix/3 Solar no fuzzy SB/WC_Sim_output_period.csv",sep=';')
+no_SB = pd.read_csv("WC_data/results/1 no SB/WC_Sim_output_period.csv",sep=';')
+no_SB_AEP = pd.read_csv("WC_data/results/1 no SB AEP/WC_Sim_output_period.csv",sep=';')
 
 #time [s]	GER pos. energy aFRR [MWh]	GER neg. energy aFRR [MWh]	GER pos. aFRR costs [EUR]	GER neg. aFRR costs [EUR]	GER pos. energy mFRR [MWh]	GER neg. energy mFRR [MWh]	GER pos. mFRR costs [EUR]	GER neg. mFRR costs [EUR]	GER AEP [EUR/MWh]
 #Feldt.index = pd.date_range(start='00:00 01.01.2019', end = '23:30 24.11.2019',freq='15 min')
@@ -45,23 +44,23 @@ Vergleich_aFRR_pow["aFRR_pow_up_no_mFRR"] = no_SB_no_mFRR["GER pos. energy aFRR 
 #correlation factor is 0.73
 Vergleich_aFRR_pow["aFRR_pow_up_sim_with_mFRR"] = no_SB_with_mFRR["GER pos. energy aFRR [MWh]"]*4
 #correlation factor is 0.85
-Vergleich_aFRR_pow["aFRR_pow_up_sim_no_SB"] = no_SB_FR["GER pos. energy aFRR [MWh]"]*4
+Vergleich_aFRR_pow["aFRR_pow_up_sim_no_SB"] = no_SB["GER pos. energy aFRR [MWh]"]*4
 #correlation factor is 0.86
-Vergleich_aFRR_pow["aFRR_pow_up_sim_fuzzySolar"] = fuzzySolar_SB_FR["GER pos. energy aFRR [MWh]"]*4
-Vergleich_aFRR_pow["aFRR_pow_up_sim_Solar"] = Solar_SB_FR["GER pos. energy aFRR [MWh]"]*4
+Vergleich_aFRR_pow["aFRR_pow_up_sim_no_SB_AEP"] = no_SB_AEP["GER pos. energy aFRR [MWh]"]*4
+#correlation factor is 0.86
 
 Vergleich_aFRR_pow["aFRR_pow_down_sim_hist"] = -hist["aFRR_down_MW"]
 Vergleich_aFRR_pow["aFRR_pow_down_no_mFRR"] = no_SB_no_mFRR["GER neg. energy aFRR [MWh]"]*(4)
 #correlation factor is 0.88
 Vergleich_aFRR_pow["aFRR_pow_down_sim_with_mFRR"] = no_SB_with_mFRR["GER neg. energy aFRR [MWh]"]*(4)
 #correlation factor is 0.90
-Vergleich_aFRR_pow["aFRR_pow_down_sim_no_SB"] = no_SB_FR["GER neg. energy aFRR [MWh]"]*(4)
+Vergleich_aFRR_pow["aFRR_pow_down_sim_no_SB"] = no_SB["GER neg. energy aFRR [MWh]"]*(4)
 #correlation factor is 0.89
-Vergleich_aFRR_pow["aFRR_pow_down_sim_fuzzySolar"] = fuzzySolar_SB_FR["GER neg. energy aFRR [MWh]"]*4
-Vergleich_aFRR_pow["aFRR_pow_down_sim_Solar"] = Solar_SB_FR["GER neg. energy aFRR [MWh]"]*4
+Vergleich_aFRR_pow["aFRR_pow_down_sim_no_SB_AEP"] = no_SB_AEP["GER neg. energy aFRR [MWh]"]*(4)
+#correlation factor is 0.89
 
 # calculate and export correlation factors to csv
-#Vergleich_aFRR_pow.corr().to_csv("validation_and_analysis/aFRR_pow_validation_via_correlation.csv",sep=';')
+#Vergleich_aFRR_pow.corr().to_csv("validation_and_analysis/corr/aFRR_pow_validation_via_correlation.csv",sep=';')
 
 #compare historic aFRR costs with simulation output via correlation factor
 Vergleich_aFRR_cost = pd.DataFrame()
@@ -70,39 +69,72 @@ Vergleich_aFRR_cost["aFRR_cost_up_sim_no_mFRR"] = no_SB_no_mFRR["GER pos. aFRR c
 #correlation factor is 0.77
 Vergleich_aFRR_cost["aFRR_cost_up_sim_with_mFRR"] = no_SB_with_mFRR["GER pos. aFRR costs [EUR]"]
 #correlation factor is 0.63
-Vergleich_aFRR_cost["aFRR_cost_up_sim_no_SB"] = no_SB_FR["GER pos. aFRR costs [EUR]"]
-#correlation factor is 0.93
+Vergleich_aFRR_cost["aFRR_cost_up_sim_no_SB"] = no_SB["GER pos. aFRR costs [EUR]"]
+#correlation factor is 0.69
+Vergleich_aFRR_cost["aFRR_cost_up_sim_no_SB_AEP"] = no_SB_AEP["GER pos. aFRR costs [EUR]"]
+#correlation factor is 0.69
 
 Vergleich_aFRR_cost["aFRR_cost_down_hist"] = hist["aFRR_down_price"]*hist['aFRR_down_MW']/(-4)
 Vergleich_aFRR_cost["aFRR_cost_down_sim_no_mFRR"] = no_SB_no_mFRR["GER neg. aFRR costs [EUR]"]
 #correlation factor is 0.77
 Vergleich_aFRR_cost["aFRR_cost_down_sim_with_mFRR"] = no_SB_with_mFRR["GER neg. aFRR costs [EUR]"]
 #correlation factor is 0.82
-Vergleich_aFRR_cost["aFRR_cost_down_sim_no_SB"] = no_SB_FR["GER neg. aFRR costs [EUR]"]
+Vergleich_aFRR_cost["aFRR_cost_down_sim_no_SB"] = no_SB["GER neg. aFRR costs [EUR]"]
+#correlation factor is 0.72
+Vergleich_aFRR_cost["aFRR_cost_down_sim_no_SB_AEP"] = no_SB_AEP["GER neg. aFRR costs [EUR]"]
 #correlation factor is 0.72
 
 # calculate and export correlation factors to csv
-#Vergleich_aFRR_cost.corr().to_csv("validation_and_analysis/aFRR_cost_validation_via_correlation.csv",sep=';')
+#Vergleich_aFRR_cost.corr().to_csv("validation_and_analysis/corr/aFRR_cost_validation_via_correlation.csv",sep=';')
 
 #____________
 Vergleich_mFRR_pow = pd.DataFrame()
 Vergleich_mFRR_pow["mFRR_pow_up_sim_hist"] = hist["mFRR_up_MW"]
 Vergleich_mFRR_pow["mFRR_pow_up_sim_with_mFRR"] = no_SB_with_mFRR["GER pos. energy mFRR [MWh]"]*4
 #correlation factor is 0.50
-Vergleich_mFRR_pow["mFRR_pow_up_sim_no_SB"] = no_SB_FR["GER pos. energy mFRR [MWh]"]*4
-#correlation factor is 0.
-
+Vergleich_mFRR_pow["mFRR_pow_up_sim_no_SB"] = no_SB["GER pos. energy mFRR [MWh]"]*4
+#correlation factor is 0.57
+Vergleich_mFRR_pow["mFRR_pow_up_sim_no_SB_AEP"] = no_SB_AEP["GER pos. energy mFRR [MWh]"]*4
+#correlation factor is 0.57
 
 Vergleich_mFRR_pow["mFRR_pow_down_sim_hist"] = -hist["mFRR_down_MW"]
 Vergleich_mFRR_pow["mFRR_pow_down_sim_with_mFRR"] = no_SB_with_mFRR["GER neg. energy mFRR [MWh]"]*(4)
 #correlation factor is 0.55
-Vergleich_mFRR_pow["mFRR_pow_down_sim_no_SB"] = no_SB_FR["GER neg. energy mFRR [MWh]"]*(4)
-#correlation factor is 0.
-
+Vergleich_mFRR_pow["mFRR_pow_down_sim_no_SB"] = no_SB["GER neg. energy mFRR [MWh]"]*(4)
+#correlation factor is 0.53
+Vergleich_mFRR_pow["mFRR_pow_down_sim_no_SB_AEP"] = no_SB_AEP["GER neg. energy mFRR [MWh]"]*(4)
+#correlation factor is 0.53
 
 # calculate and export correlation factors to csv
-#Vergleich_mFRR_pow.corr().to_csv("validation_and_analysis/mFRR_pow_validation_via_correlation.csv",sep=';')
+#Vergleich_mFRR_pow.corr().to_csv("validation_and_analysis/corr/mFRR_pow_validation_via_correlation.csv",sep=';')
 #_______________
+
+#compare historic aFRR costs with simulation output via correlation factor
+Vergleich_mFRR_cost = pd.DataFrame()
+Vergleich_mFRR_cost["mFRR_cost_up_hist"] = hist["mFRR_up_price"]*hist['mFRR_up_MW']/4
+Vergleich_mFRR_cost["mFRR_cost_up_sim_no_mFRR"] = no_SB_no_mFRR["GER pos. mFRR costs [EUR]"]
+#correlation factor is -
+Vergleich_mFRR_cost["mFRR_cost_up_sim_with_mFRR"] = no_SB_with_mFRR["GER pos. mFRR costs [EUR]"]
+#correlation factor is 0.51
+Vergleich_mFRR_cost["mFRR_cost_up_sim_no_SB"] = no_SB["GER pos. mFRR costs [EUR]"]
+#correlation factor is 0.57
+Vergleich_mFRR_cost["mFRR_cost_up_sim_no_SB_AEP"] = no_SB_AEP["GER pos. mFRR costs [EUR]"]
+#correlation factor is 0.57
+
+Vergleich_mFRR_cost["mFRR_cost_down_hist"] = hist["mFRR_down_price"]*hist['mFRR_down_MW']/(-4)
+Vergleich_mFRR_cost["mFRR_cost_down_sim_no_mFRR"] = no_SB_no_mFRR["GER neg. mFRR costs [EUR]"]
+#correlation factor is -
+Vergleich_mFRR_cost["mFRR_cost_down_sim_with_mFRR"] = -no_SB_with_mFRR["GER neg. mFRR costs [EUR]"]
+#correlation factor is 0.23
+Vergleich_mFRR_cost["mFRR_cost_down_sim_no_SB"] = -no_SB["GER neg. mFRR costs [EUR]"]
+#correlation factor is 0.32
+Vergleich_mFRR_cost["mFRR_cost_down_sim_no_SB_AEP"] = -no_SB_AEP["GER neg. mFRR costs [EUR]"]
+#correlation factor is 0.32
+
+# calculate and export correlation factors to csv
+#Vergleich_mFRR_cost.corr().to_csv("validation_and_analysis/corr/mFRR_cost_validation_via_correlation.csv",sep=';')
+
+
 
 #compare historic imbalance price with simulation output via correlation factor
 Vergleich_AEP_imbalance_price = pd.DataFrame()
@@ -111,11 +143,13 @@ Vergleich_AEP_imbalance_price["imbalance_price_Euro_per_MWh_sim_no_mFRR"] = no_S
 #correlation factor is 0.48
 Vergleich_AEP_imbalance_price["imbalance_price_Euro_per_MWh_sim_with_mFRR"] = no_SB_with_mFRR["GER AEP [EUR/MWh]"]
 #correlation factor is 0.37
-Vergleich_AEP_imbalance_price["imbalance_price_Euro_per_MWh_sim_FR"] = no_SB_FR["GER AEP [EUR/MWh]"]
+Vergleich_AEP_imbalance_price["imbalance_price_Euro_per_MWh_sim_FR"] = no_SB["GER AEP [EUR/MWh]"]
 #correlation factor is 0.37
+Vergleich_AEP_imbalance_price["imbalance_price_Euro_per_MWh_sim_FR_AEP"] = no_SB_AEP["GER AEP [EUR/MWh]"]
+#correlation factor is 0.52
 
 # calculate and export correlation factors to csv
-#Vergleich_AEP_imbalance_price.corr().to_csv("validation_and_analysis/AEP_imbalance_price_validation_via_correlation.csv",sep=';')
+#Vergleich_AEP_imbalance_price.corr().to_csv("validation_and_analysis/corr/AEP_imbalance_price_validation_via_correlation.csv",sep=';')
 
 
 
